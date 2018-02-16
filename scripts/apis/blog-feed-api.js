2ce97bfa26b685328e3da4169e10a702e48f46aa
Max <herrschwartz@ya.ru>
2018-02-16 22:28:52 +0300

    1

--- scripts/apis/blog-feed-api.js
+++ scripts/apis/blog-feed-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const BlogMap = {
-  'graphic-flip': 'https://graphicflip.com/feed/',
-  'jourtify': 'https://www.theguardian.com/international/rss',
+  'graphic-flip': 'https://forklog.com/feed/',
+  'jourtify': 'https://jourtify.com/feed/',
   'sdr': 'https://superdevresources.com/feed/',
   'template-flip' : 'https://templateflip.com/feed/',
   'awwwards': 'http://www.awwwards.com/blog/feed/',
