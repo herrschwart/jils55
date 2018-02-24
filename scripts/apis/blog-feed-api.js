2a8a90f0072bb30c3fb28e297afd2c866b577bae
Max <herrschwartz@ya.ru>
2018-02-24 21:58:48 +0300

    1
    1

--- scripts/apis/blog-feed-api.js
+++ scripts/apis/blog-feed-api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const BlogMap = {
   'graphic-flip': 'https://graphicflip.com/feed/',
-  'jourtify': 'http://feeds.feedburner.com/Jourtify',
+  'jourtify': 'http://feeds2.feedburner.com/Jourtify',
   'sdr': 'https://superdevresources.com/feed/',
   'template-flip' : 'https://jourtify.com/feed',
   'awwwards': 'http://www.awwwards.com/blog/feed/',
